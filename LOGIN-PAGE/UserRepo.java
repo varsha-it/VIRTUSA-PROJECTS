4ee82773109fc2a65a0c3d08287610fe0946f7a1
varsha-it <113794449+varsha-it@users.noreply.github.com>
2022-11-24 02:06:14 +0530

    Update UserRepo.java

--- LOGIN-PAGE/UserRepo.java
+++ LOGIN-PAGE/UserRepo.java
@@ -1,1 +1,9 @@
 
+package com.app.repo;
+import org.springframework.data.jpa.repository.jpaRepository;
+import com.app.model.user;
+@Repository
+
+public interface UserRepo extends JpaRepository<User,String>{
+
+}
