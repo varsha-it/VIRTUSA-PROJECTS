e6616570e4933a4099553a10de126570379dc4c5
varsha-it <113794449+varsha-it@users.noreply.github.com>
2022-11-24 06:01:42 +0530

    Delete LOGIN-PAGE directory

--- LOGIN-PAGE/UserRepo.java
+++
@@ -1,9 +0,0 @@
-
-package com.app.repo;
-import org.springframework.data.jpa.repository.jpaRepository;
-import com.app.model.user;
-@Repository
-
-public interface UserRepo extends JpaRepository<User,String>{
-
-}
